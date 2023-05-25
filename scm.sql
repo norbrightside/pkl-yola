f654fadc44e748b2e21e9c8a9c653a8bea125708
norbrightside <122191874+norbrightside@users.noreply.github.com>
2023-05-25 22:54:18 +0700

    update scm

--- scm.sql
+++ scm.sql
@@ -1,38 +1,187 @@
-CREATE DATABASE IF NOT EXISTS SCM;
- USE SCM;
- CREATE TABLE IF NOT EXISTS users (
-     INT PRIMARY KEY AUTO_INCREMENT,
-    username VARCHAR(8) NOT NULL,
-    password VARCHAR(8) NOT NULL;
- CREATE TABLE IF NOT EXISTS suppliers (
-    supplier_id INT PRIMARY KEY AUTO_INCREMENT,
-    supplier_name VARCHAR(255) NOT NULL,
-    supplier_address VARCHAR(255) NOT NULL,
-    supplier_contact VARCHAR(255) NOT NULL
-);
- CREATE TABLE IF NOT EXISTS raw_materials (
-    material_id INT PRIMARY KEY AUTO_INCREMENT,
-    material_name VARCHAR(255) NOT NULL,
-    material_unit VARCHAR(255) NOT NULL,
-    material_unit_price DECIMAL(10,2) NOT NULL
-);
- CREATE TABLE IF NOT EXISTS productions (
-    production_id INT PRIMARY KEY AUTO_INCREMENT,
-    production_date DATE NOT NULL,
-    material_id INT NOT NULL,
-    production_qty INT NOT NULL,
-    FOREIGN KEY (material_id) REFERENCES raw_materials(material_id)
-);
- CREATE TABLE IF NOT EXISTS sales (
-    sale_id INT PRIMARY KEY AUTO_INCREMENT,
-    sale_date DATE NOT NULL,
-    material_id INT NOT NULL,
-    sale_qty INT NOT NULL,
-    FOREIGN KEY (material_id) REFERENCES raw_materials(material_id)
-);
- CREATE TABLE IF NOT EXISTS stock (
-    stock_id INT PRIMARY KEY AUTO_INCREMENT,
-    material_id INT NOT NULL,
-    stock_qty INT NOT NULL,
-    FOREIGN KEY (material_id) REFERENCES raw_materials(material_id)
-);
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 25, 2023 at 05:53 PM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 7.4.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `scm`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `productions`
+--
+
+CREATE TABLE `productions` (
+  `idproduk` int(11) NOT NULL,
+  `material_id` int(11) NOT NULL,
+  `stok` int(11) NOT NULL,
+  `harga` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `raw_materials`
+--
+
+CREATE TABLE `raw_materials` (
+  `material_id` int(11) NOT NULL,
+  `material_name` varchar(255) NOT NULL,
+  `material_unit` varchar(255) NOT NULL,
+  `material_unit_price` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `raw_materials`
+--
+
+INSERT INTO `raw_materials` (`material_id`, `material_name`, `material_unit`, `material_unit_price`) VALUES
+(1, 'rotan', '25', '150000.00'),
+(2, '', '', '15000.00'),
+(12, 'asd', '12', '15000.00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sales`
+--
+
+CREATE TABLE `sales` (
+  `sale_id` int(11) NOT NULL,
+  `sale_date` date NOT NULL,
+  `material_id` int(11) NOT NULL,
+  `sale_qty` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `suppliers`
+--
+
+CREATE TABLE `suppliers` (
+  `supplier_id` int(11) NOT NULL,
+  `supplier_name` varchar(255) NOT NULL,
+  `supplier_address` varchar(255) NOT NULL,
+  `supplier_contact` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `suppliers`
+--
+
+INSERT INTO `suppliers` (`supplier_id`, `supplier_name`, `supplier_address`, `supplier_contact`) VALUES
+(1, 'dodi', 'halaban', '081212121212');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `username` varchar(8) NOT NULL,
+  `password` varchar(8) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`username`, `password`) VALUES
+('admin', 'admin');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `productions`
+--
+ALTER TABLE `productions`
+  ADD PRIMARY KEY (`idproduk`),
+  ADD KEY `material_id` (`material_id`);
+
+--
+-- Indexes for table `raw_materials`
+--
+ALTER TABLE `raw_materials`
+  ADD PRIMARY KEY (`material_id`);
+
+--
+-- Indexes for table `sales`
+--
+ALTER TABLE `sales`
+  ADD PRIMARY KEY (`sale_id`),
+  ADD KEY `material_id` (`material_id`);
+
+--
+-- Indexes for table `suppliers`
+--
+ALTER TABLE `suppliers`
+  ADD PRIMARY KEY (`supplier_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `productions`
+--
+ALTER TABLE `productions`
+  MODIFY `idproduk` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `raw_materials`
+--
+ALTER TABLE `raw_materials`
+  MODIFY `material_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `sales`
+--
+ALTER TABLE `sales`
+  MODIFY `sale_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `suppliers`
+--
+ALTER TABLE `suppliers`
+  MODIFY `supplier_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `productions`
+--
+ALTER TABLE `productions`
+  ADD CONSTRAINT `productions_ibfk_1` FOREIGN KEY (`material_id`) REFERENCES `raw_materials` (`material_id`);
+
+--
+-- Constraints for table `sales`
+--
+ALTER TABLE `sales`
+  ADD CONSTRAINT `sales_ibfk_1` FOREIGN KEY (`material_id`) REFERENCES `raw_materials` (`material_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
